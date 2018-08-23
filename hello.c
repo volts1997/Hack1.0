596f670be6e2ff28f03af94810223c7f0377e95d
volts1997 <42520091+volts1997@users.noreply.github.com>
2018-08-24 01:04:29 +0800

    Chai yen commented

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Hello World!\n");
+  printf("Hing Chai Yen,Computer Science\n");
 
   return 0;
 }
---
+++ readme.md.txt
@@ -0,0 +1,3 @@
+Name: Hing Chai Yen
+Major: Computer Science
+https://github.com/volts1997/Hack1.0.git
