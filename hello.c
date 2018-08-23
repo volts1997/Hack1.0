773b79fcc64fc752febcf5f90654078aa3d7b255
volts1997 <42520091+volts1997@users.noreply.github.com>
2018-08-24 00:35:38 +0800

    Add hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Name: Hing Chai Yen
+ * Major: Computer Science
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
